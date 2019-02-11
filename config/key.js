23badb9f0530bb52483a8933ddb0c4c7d6a588a8
Aman Kumar Singh <er.aks473@gmail.com>
2019-02-12 01:54:11 +0530

    UPDATE

--- config/key.js
+++ config/key.js
@@ -1,11 +1,11 @@
 module.exports= {
     google: {
-        clientID: '598165447701-ddugnb99kglj5oasn051hub063n7m4ft.apps.googleusercontent.com',
-        clientSecret: '_txldQT92f7gbmmQ1aLSWUOc'
+        clientID: CLIENT_ID,
+        clientSecret: CLIENT_SECRET
     },
 
     mongodb: {
-        dbURI: 'mongodb://oauthclient:oauth12345@ds247007.mlab.com:47007/aman-oauth-test'
+        dbURI: 'mongodb://oauthclient:<PASSWORD>@ds247007.mlab.com:47007/aman-oauth-test'
     },
 
     session: {
